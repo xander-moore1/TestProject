00a9ae95cdd6bec8d3da11671bfd33a1912179db
XJM <xander_moore1@mymail.eku.edu>
2026-01-20 12:16:04 -0500

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Moore, Xander J. on 1/20/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
